a5b1dee12e19236d8ea48bca277bdead26a77f44
christie <cbrandao18@gmail.com>
2019-07-24 17:44:08 -0400

    create auth route and use it for the login form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Route, Switch } from 'react-router-dom';
 import ProductIndex from './products/ProductIndex'
 import Login from './Login';
+import AuthRoute from '../util/route_util';
 
 const App = () => {
   return (
     <div>
       <h1>Online Store</h1>
       <Switch>
-        <Route exact path="/login" component={Login} />
+        <AuthRoute exact path="/login" component={Login} routeType="auth"/>
         <Route path="/" component={ProductIndex} />
       </Switch>
     </div>
